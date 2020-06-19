d81a4a1370a4c2914fbe5ec8c3b241a542a7c367
Alexey <lukomorie2001@gmail.com>
2020-06-19 19:04:59 +0700

    add 'getting started' guides

---
+++ getting started guides/springBoot_restHelloWorld/src/test/java/com/agrokhotov/springBoot_restHelloWorld/SpringBootRestHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrokhotov.springBoot_restHelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
