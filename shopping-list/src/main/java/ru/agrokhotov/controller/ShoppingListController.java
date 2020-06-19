becc11eec28f7fdb52453029c4f3bf1aeb1f8c03
Alexey <lukomorie2001@gmail.com>
2020-06-19 18:56:47 +0700

    adding and deleting items to shopping list

---
+++ shopping-list/src/main/java/ru/agrokhotov/controller/ShoppingListController.java
@@ -0,0 +1,44 @@
+package ru.agrokhotov.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.agrokhotov.persist.ShoppingItem;
+import ru.agrokhotov.persist.ShoppingItemRepository;
+
+@Controller
+public class ShoppingListController {
+
+    private final ShoppingItemRepository repository;
+
+    @Autowired
+    public ShoppingListController(ShoppingItemRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public String indexPage(Model model) {
+        // pass all items to the model
+        model.addAttribute("items", repository.findAll());
+
+        // creating a new item
+        model.addAttribute("item", new ShoppingItem());
+        return "index";
+    }
+
+    @PostMapping
+    public String newShoppingItem(ShoppingItem item) {
+        repository.save(item);
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteShoppingItem(@PathVariable("id") Long id) {
+        repository.deleteById(id);
+        return "redirect:/";
+    }
+}
---
+++ shopping-list/src/main/java/ru/agrokhotov/persist/ShoppingItemRepository.java
@@ -0,0 +1,8 @@
+package ru.agrokhotov.persist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShoppingItemRepository extends JpaRepository<ShoppingItem, Long> {
+}
