59a3d51f6ca08ffc3bf34e8a2b518e5cc4d3203b
Alexey <lukomorie2001@gmail.com>
2020-06-22 12:09:08 +0700

    connected user to a shopping list

--- shopping-list/src/main/java/ru/agrokhotov/controller/ShoppingListController.java
+++ shopping-list/src/main/java/ru/agrokhotov/controller/ShoppingListController.java
@@ -1,5 +1,7 @@
 package ru.agrokhotov.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,21 +11,30 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.agrokhotov.persist.ShoppingItem;
 import ru.agrokhotov.persist.ShoppingItemRepository;
+import ru.agrokhotov.persist.User;
+import ru.agrokhotov.persist.UserRepository;
+
+import java.security.Principal;
 
 @Controller
 public class ShoppingListController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ShoppingListController.class);
     private final ShoppingItemRepository repository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public ShoppingListController(ShoppingItemRepository repository) {
+    public ShoppingListController(ShoppingItemRepository repository, UserRepository userRepository) {
         this.repository = repository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping
-    public String indexPage(Model model) {
-        // pass all items to the model
-        model.addAttribute("items", repository.findAll());
+    public String indexPage(Model model, Principal principal) {
+        logger.info("User name: {}", principal.getName());
+
+        // pass items to the model according to username
+        model.addAttribute("items", repository.findByUserUsername(principal.getName()));
 
         // creating a new item
         model.addAttribute("item", new ShoppingItem());
@@ -31,7 +42,10 @@ public class ShoppingListController {
     }
 
     @PostMapping
-    public String newShoppingItem(ShoppingItem item) {
+    public String newShoppingItem(ShoppingItem item, Principal principal) {
+        logger.info("User name: {}", principal.getName());
+        User user = userRepository.findByUsername(principal.getName()).get();
+        item.setUser(user);
         repository.save(item);
         return "redirect:/";
     }
--- shopping-list/src/main/java/ru/agrokhotov/persist/ShoppingItemRepository.java
+++ shopping-list/src/main/java/ru/agrokhotov/persist/ShoppingItemRepository.java
@@ -3,6 +3,9 @@ package ru.agrokhotov.persist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ShoppingItemRepository extends JpaRepository<ShoppingItem, Long> {
+    List<ShoppingItem> findByUserUsername(String username);
 }
