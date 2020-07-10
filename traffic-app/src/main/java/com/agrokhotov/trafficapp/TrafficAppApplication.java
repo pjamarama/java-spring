34741ae5ed8633d7d7ae4728288e9050f1e6336c
Alexey <lukomorie2001@gmail.com>
2020-07-10 22:29:31 +0300

    initial commit

---
+++ traffic-app/src/main/java/com/agrokhotov/trafficapp/TrafficAppApplication.java
@@ -0,0 +1,13 @@
+package com.agrokhotov.trafficapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficAppApplication.class, args);
+	}
+
+}
